3904e36c2db4c3a3ee2ac41b5cf13ad9b8d29cc1
Pankaj Kumar Mandal <pankajkrp16@.com>
2025-08-12 23:19:48 +0530

    Create a new file and check the output

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-    <>
-      <p>hello</p>
-    </>
+    <div>
+    <Header/>
+    <h1>Hello React</h1>
+    </div>
+    
   )
 }
 
-export default App
+export default App;
